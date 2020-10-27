eaa63a856634fc9b072ec059aae99c3e7739d351
Matheus Porto <matheusportoo@gmail.com>
2020-10-26 23:15:42 -0300

    initial structure

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+h1 {
+  margin: 40px 0 0;
+}
+</style>
